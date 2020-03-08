b4bd52a09b2d5dafe6467e84ed5d95259afe0cdf
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-08 03:48:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown (number)
+  number = 10
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  if number == 0
+  return "HAPPY NEW YEAR!"
+end
+end
 end
