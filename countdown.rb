f587cace110e1f7a5d29223013e7db703a43d42d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-08 03:55:58 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  seconds = 10
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+  if seconds == 0
+  "HAPPY NEW YEAR!"
+end
+end
+
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+  puts " #{seconds} SECOND(S)!"
+  sleep 1
+  seconds -= 1
+end
+return "HAPPY NEW YEAR!"
 end
